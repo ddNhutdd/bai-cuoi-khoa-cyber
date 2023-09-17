e02735d66b17cb0fb18779a087e7aed2901b00f6
Nganha203 <nguyenngocnganha203@gmail.com>
2023-09-17 14:41:17 +0700

    update-1

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Suspense, lazy } from 'react'
 import UserTemplate from '../templates/user/user.template'
+
+const Home = lazy(() => import('../pages/home/home'));
+const DanhMuc = lazy(() => import("../pages/danh-muc/danh-muc"));
+const ChiTiet = lazy(() => import("../pages/chi-tiet/chi-tiet"));
+const Register = lazy(() => import("../pages/register"));
+const Login = lazy(() => import("../pages/login"));
+
 export const router = createBrowserRouter([
     {
         element: <UserTemplate />,
@@ -13,6 +20,38 @@ export const router = createBrowserRouter([
                     </Suspense>
                 ),
             },
+            {
+                path: '/danhmuckhoahoc',
+                element: (
+                    <Suspense>
+                        <DanhMuc />
+                    </Suspense>
+                ),
+            },
+            {
+                path: '/chitiet',
+                element: (
+                    <Suspense>
+                        <ChiTiet />
+                    </Suspense>
+                ),
+            },
+            {
+                path: '/register',
+                element: (
+                    <Suspense>
+                        <Register/>
+                    </Suspense>
+                ),
+            },
+            {
+                path: '/login',
+                element: (
+                    <Suspense>
+                        <Login/>
+                    </Suspense>
+                ),
+            }
         ],
     },
     {
